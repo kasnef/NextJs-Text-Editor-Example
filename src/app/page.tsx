f615acd9e1819a5e69df1cc7c0bf1edfe408e372
Ca Lang Thang <90557694+kasnef@users.noreply.github.com>
2025-04-07 22:35:26 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TextEditor from "./components/text.editor";
 export default function CreateBlog() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">WYSIWYG Editor with Tiptap (Template)</h1>
+      <h1 className="text-2xl font-bold mb-4">WYSIWYG Editor with Tiptap (Example)</h1>
       <div className="w-full h-full bg-white rounded-lg p-4">
         <TextEditor />
       </div>
